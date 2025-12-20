d682a672b65f690c61360e7e12c0c9b001db0608
Chad Smith <chad.e.smith@outlook.com>
2025-12-20 17:33:12 -0600

    feat: implement header component with toolbar and menu

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'pirates-dred-header',
-  imports: [],
+  imports: [
+    MatButton,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbar,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
