5d575d53f478c2b514fce4abdaf33d128bb9204e
Chad Smith <chad.e.smith@outlook.com>
2026-01-03 22:57:27 -0600

    feat(config): add animations and form field options to application configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideAnimationsAsync(),
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline', floatLabel: 'always'}}
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: 'character', loadComponent: () => import('../components/character/character.component').then(c => c.CharacterComponent) },
+  { path: 'character/create', loadComponent: () => import('../components/character/create-character/create-character.component').then(c => c.CreateCharacterComponent) },
 ];
